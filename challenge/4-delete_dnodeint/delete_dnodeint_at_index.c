e29f13594a3853c309420f5767e261a6d4079efc
JorgeBeritan <inaikmcontacto@gmail.com>
2024-09-12 10:06:41 -0300

    First commit

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,43 +11,41 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *current = *head;
+    unsigned int p = 0;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    if (*head == NULL)
+        return (-1);
+
+    /* Traverse to the node at the specified index */
+    while (current != NULL && p < index)
+    {
+        current = current->next;
+        p++;
+    }
+
+    /* If we reached the end of the list or the index was out of range */
+    if (current == NULL)
+        return (-1);
+
+    /* If the node to delete is the first node (head) */
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        /* Update the previous node's next pointer */
+        if (current->prev != NULL)
+            current->prev->next = current->next;
+
+        /* Update the next node's prev pointer */
+        if (current->next != NULL)
+            current->next->prev = current->prev;
+    }
+
+    free(current);
+    return (1);
 }
